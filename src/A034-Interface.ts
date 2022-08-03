e4873f7afa908d2073f1e2d30be4591ca78fedd8
Leeo.exe <leonardo.basso147@gmail.com>
2022-08-03 15:46:52 +0100

    InterfaceTypeScript

---
+++ src/A034-Interface.ts
@@ -0,0 +1,16 @@
+interface TypePeople {
+  name: string;
+  surname: string;
+  fullName(): string;
+}
+
+export class People implements TypePeople {
+  constructor(public name: string, public surname: string) {}
+
+  fullName(): string {
+    return this.name + this.surname;
+  }
+}
+
+const people = new People('leo ', 'basso');
+console.log(people.fullName());
